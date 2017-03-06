2e028ea57e1941e0acfab6f7302b0d527578c8a5
powerofdeen <powerofdeen@naver.com>
2017-03-06 17:33:38 +0900

    study project create

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by powerofdeen on 2017. 3. 6..
+//  Copyright © 2017년 powerofdeen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
